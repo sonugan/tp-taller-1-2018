318236e0958f8489cc8cc579927caa4f20c1e572
Sebastián Wandelow <swandelow@despegar.com>
2018-03-28 22:01:35 -0300

    agrego un parser básico de yaml

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "game/game.h"
+#include "configuration-parser.h"
+
 
 int main( int argc, char* args[] ) {
+
+    ConfigurationParser* parser = new ConfigurationParser();
+    parser->ReadFile("aaa");
+
     Game* game = new Game();
     game->Start();
     game->End();
