70a3f5561f372fed1c10aff0eb95270eccbd5269
Sebastián Wandelow <swandelow@despegar.com>
2018-05-30 02:01:13 -0300

    fix gamecontroller: solo enviar quit request si el cliente estaba conectado.

--- src/client/controller/game-controller.cpp
+++ src/client/controller/game-controller.cpp
@@ -11,8 +11,11 @@ GameController::~GameController() {
 
 void GameController::ExitGame(const Uint8 *keyboard_state_array) {
     if (keyboard_state_array[SDL_SCANCODE_ESCAPE]) {
-        QuitRequest* quit_request = new QuitRequest(game->GetUser()->GetUsername());
-        client->Quit(quit_request);
+		if (client->IsConnected())
+		{
+			QuitRequest* quit_request = new QuitRequest(game->GetUser()->GetUsername());
+			client->Quit(quit_request);
+		}
     }
 }
 
