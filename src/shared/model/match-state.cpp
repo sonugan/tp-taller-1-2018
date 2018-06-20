b834fe770348d6dbb70715406fa7f4f4abb142a0
Sebastián Wandelow <swandelow@despegar.com>
2018-06-20 17:42:22 -0300

    agrego metodos para manipular el estado del match

--- src/shared/model/match-state.cpp
+++ src/shared/model/match-state.cpp
@@ -10,6 +10,7 @@
 MatchState::MatchState()
 {
 	this->type = MATCH_STATE_TYPE::KICKOFF;
+	this->active_kickoff_team = TEAM_NUMBER::TEAM_A; // esto es arbitrario. cambiar si hace falta.
 }
 
 MatchState::~MatchState()
@@ -21,4 +22,27 @@ MATCH_STATE_TYPE MatchState::GetType(){
 
 bool MatchState::IsKickOff() {
 	return MATCH_STATE_TYPE::KICKOFF == GetType();
+}
+
+void MatchState::SetKickOff(TEAM_NUMBER active_kickoff_team) {
+	this->type=MATCH_STATE_TYPE::KICKOFF;
+	this->active_kickoff_team = active_kickoff_team;
+}
+
+void MatchState::SetPlaying() {
+	this->type = MATCH_STATE_TYPE::PLAYING;
+}
+
+void MatchState::SetGoal(TEAM_NUMBER goal_scorer_team) {
+	this->type = MATCH_STATE_TYPE::GOAL;
+	this->goal_scorer_team = goal_scorer_team;
+}
+
+void MatchState::SetGoalKick(TEAM_NUMBER goal_kick_team) {
+	this->type = MATCH_STATE_TYPE::GOAL_KICK;
+	this->goal_kick_team = goal_kick_team;
+}
+
+void MatchState::SetTimeup() {
+	this->type = MATCH_STATE_TYPE::TIME_UP;
 }
--- src/shared/model/match-state.h
+++ src/shared/model/match-state.h
@@ -9,6 +9,7 @@
 #define SRC_SHARED_MODEL_MATCH_STATE_H_
 
 #include "match-state-type.h"
+#include "team-number.h"
 
 class MatchState
 {
@@ -17,9 +18,18 @@ public:
 	virtual ~MatchState();
 	MATCH_STATE_TYPE GetType();
 
+	void SetKickOff(TEAM_NUMBER active_kickoff_team);
 	bool IsKickOff();
+	void SetPlaying();
+	void SetGoal(TEAM_NUMBER goal_scorer_team);
+	void SetGoalKick(TEAM_NUMBER goal_kick_team);
+	void SetTimeup();
+
 private:
 	MATCH_STATE_TYPE type;
+	TEAM_NUMBER active_kickoff_team;
+	TEAM_NUMBER goal_scorer_team;
+	TEAM_NUMBER goal_kick_team;
 };
 
 #endif /* SRC_SHARED_MODEL_MATCH_STATE_H_ */
