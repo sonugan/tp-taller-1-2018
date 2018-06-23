8d96e2927cd53b45ec73f48f7e86e80ff61099fb
pera21 <pereira.martin21@gmail.com>
2018-06-23 20:07:03 -0300

    Se arregla la animacion de la pelota por el aire

--- src/client/view/ball-view.cpp
+++ src/client/view/ball-view.cpp
@@ -49,99 +49,89 @@ BallView::BallView(Ball* ball)
 
     animations.push_back(new Animation("ball_still", ball_still_clips, 0.3));
 
-    std::vector<SDL_Rect*> upwards_ball_clips;
+    std::vector<SDL_Rect*> upwards_ball_clips1;
     SDL_Rect* sprite6 = new SDL_Rect();
     sprite6->x = 340;
     sprite6->y = 167;
     sprite6->w = 20;
     sprite6->h = 20;
 
+    std::vector<SDL_Rect*> upwards_ball_clips2;
     SDL_Rect* sprite7 = new SDL_Rect();
     sprite7->x = 378;
     sprite7->y = 168;
     sprite7->w = 22;
     sprite7->h = 22;
 
+    std::vector<SDL_Rect*> upwards_ball_clips3;
     SDL_Rect* sprite8 = new SDL_Rect();
-    sprite8->x = 458;
-    sprite8->y = 165;
-    sprite8->w = 25;
-    sprite8->h = 25;
+    sprite8->x = 415;
+    sprite8->y = 168;
+    sprite8->w = 23;
+    sprite8->h = 23;
 
+    std::vector<SDL_Rect*> upwards_ball_clips4;
     SDL_Rect* sprite9 = new SDL_Rect();
-    sprite9->x = 538;
-    sprite9->y = 163;
-    sprite9->w = 27;
-    sprite9->h = 27;
+    sprite9->x = 458;
+    sprite9->y = 165;
+    sprite9->w = 25;
+    sprite9->h = 25;
 
+    std::vector<SDL_Rect*> upwards_ball_clips5;
     SDL_Rect* sprite10 = new SDL_Rect();
-    sprite10->x = 573;
-    sprite10->y = 160;
-    sprite10->w = 30;
-    sprite10->h = 30;
+    sprite10->x = 498;
+    sprite10->y = 165;
+    sprite10->w = 27;
+    sprite10->h = 27;
 
+    std::vector<SDL_Rect*> upwards_ball_clips6;
     SDL_Rect* sprite11 = new SDL_Rect();
-    sprite11->x = 655;
-    sprite11->y = 158;
-    sprite11->w = 32;
-    sprite11->h = 32;
+    sprite11->x = 538;
+    sprite11->y = 163;
+    sprite11->w = 29;
+    sprite11->h = 29;
 
-    upwards_ball_clips.push_back(sprite6);
-    upwards_ball_clips.push_back(sprite7);
-    upwards_ball_clips.push_back(sprite8);
-    upwards_ball_clips.push_back(sprite9);
-    upwards_ball_clips.push_back(sprite10);
-    upwards_ball_clips.push_back(sprite11);
-
-    unsigned int ball_speed = ball->GetTrajectory()->GetBallSpeed();
-    animations.push_back(new Animation("upwards_ball", upwards_ball_clips, 0.7));
-
-    std::vector<SDL_Rect*> downwards_ball_clips;
-
+    std::vector<SDL_Rect*> upwards_ball_clips7;
     SDL_Rect* sprite12 = new SDL_Rect();
-    sprite12->x = 655;
-    sprite12->y = 158;
+    sprite12->x = 574;
+    sprite12->y = 161;
     sprite12->w = 32;
     sprite12->h = 32;
 
+    std::vector<SDL_Rect*> upwards_ball_clips8;
     SDL_Rect* sprite13 = new SDL_Rect();
-    sprite13->x = 573;
+    sprite13->x = 612;
     sprite13->y = 160;
-    sprite13->w = 30;
-    sprite13->h = 30;
+    sprite13->w = 34;
+    sprite13->h = 34;
 
+    std::vector<SDL_Rect*> upwards_ball_clips9;
     SDL_Rect* sprite14 = new SDL_Rect();
-    sprite14->x = 538;
-    sprite14->y = 163;
-    sprite14->w = 27;
-    sprite14->h = 27;
+    sprite14->x = 654;
+    sprite14->y = 158;
+    sprite14->w = 36;
+    sprite14->h = 36;
 
-    SDL_Rect* sprite15 = new SDL_Rect();
-    sprite15->x = 458;
-    sprite15->y = 165;
-    sprite15->w = 25;
-    sprite15->h = 25;
+    upwards_ball_clips1.push_back(sprite6);
+    upwards_ball_clips2.push_back(sprite7);
+    upwards_ball_clips3.push_back(sprite8);
+    upwards_ball_clips4.push_back(sprite9);
+    upwards_ball_clips5.push_back(sprite10);
+    upwards_ball_clips6.push_back(sprite11);
+    upwards_ball_clips7.push_back(sprite12);
+    upwards_ball_clips8.push_back(sprite13);
+    upwards_ball_clips9.push_back(sprite14);
 
-    SDL_Rect* sprite16 = new SDL_Rect();
-    sprite16->x = 378;
-    sprite16->y = 168;
-    sprite16->w = 22;
-    sprite16->h = 22;
 
-    SDL_Rect* sprite17 = new SDL_Rect();
-    sprite17->x = 340;
-    sprite17->y = 167;
-    sprite17->w = 20;
-    sprite17->h = 20;
-
-    downwards_ball_clips.push_back(sprite12);
-    downwards_ball_clips.push_back(sprite13);
-    downwards_ball_clips.push_back(sprite14);
-    downwards_ball_clips.push_back(sprite15);
-    downwards_ball_clips.push_back(sprite16);
-    downwards_ball_clips.push_back(sprite17);
-
-    animations.push_back(new Animation("downwards_ball", downwards_ball_clips, 0.6));
+    animations.push_back(new Animation("upwards_ball1", upwards_ball_clips1, 1));
+    animations.push_back(new Animation("upwards_ball2", upwards_ball_clips2, 1));
+    animations.push_back(new Animation("upwards_ball3", upwards_ball_clips3, 1));
+    animations.push_back(new Animation("upwards_ball4", upwards_ball_clips4, 1));
+    animations.push_back(new Animation("upwards_ball5", upwards_ball_clips5, 1));
+    animations.push_back(new Animation("upwards_ball6", upwards_ball_clips6, 1));
+    animations.push_back(new Animation("upwards_ball7", upwards_ball_clips7, 1));
+    animations.push_back(new Animation("upwards_ball8", upwards_ball_clips8, 1));
+    animations.push_back(new Animation("upwards_ball9", upwards_ball_clips9, 1));
 
     current_animation_index = 0;
 
@@ -164,17 +154,32 @@ BallView::~BallView()
 
 void BallView::Render(int x_camera, int y_camera, int max_x, int max_y) {
 
-    if(IsStill()) {
+    int ball_z = ball->GetLocation()->GetZ();
+
+
+    if ((ball_z > 0) && (ball_z < 20)) {
+        current_animation_index = 2;
+    }else if ((ball_z >= 20) && (ball_z < 40)){
+        current_animation_index = 3;
+    } else if ((ball_z >= 40) && (ball_z < 80)) {
+        current_animation_index = 4;
+    }else if ((ball_z >= 80) && (ball_z < 110)){
+        current_animation_index = 5;
+    } else if ((ball_z >= 110) && (ball_z < 150)) {
+        current_animation_index = 6;
+    }else if ((ball_z >= 150) && (ball_z < 180)){
+        current_animation_index = 7;
+    }else if ((ball_z >= 180) && (ball_z < 210)){
+        current_animation_index = 8;
+    }else if ((ball_z >= 210) && (ball_z < 240)){
+        current_animation_index = 9;
+    }else if (ball_z >= 240){
+        current_animation_index = 10;
+    }else if(IsStill()) {
         current_animation_index = 1;
         Logger::getInstance()->info("BALL IS STILL");
-    } else if (this->GoingUp()) {
-        current_animation_index = 2;
-    }else if (this->GoingDown()) {
-        current_animation_index = 3;
     }else{
         current_animation_index = 0;
-        this->animations[2]->Restart();
-        this->animations[3]->Restart();
     }
 
     SDL_Rect* current_clip = this->animations[current_animation_index]->NextClip();
